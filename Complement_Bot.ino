48798d9c766272e90302c920124d65b5a0713617
varunak <varunkuthe@live.com>
2019-05-30 12:11:43 +0545

    Add files via upload

--- Complement_Bot.ino
+++ Complement_Bot.ino
@@ -9,79 +9,86 @@ int distance;
 int randNumber;
 
 void setup() {
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-pinMode(LEDpin, OUTPUT);
-Serial.begin(9600); // Starts the serial communication
-lcd.begin(16, 2);    
-randomSeed(analogRead(0));
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(LEDpin, OUTPUT);
+  Serial.begin(9600); // Starts the serial communication
+  lcd.begin(16, 2);
+  randomSeed(analogRead(0));
 }
 void loop() {
-  
-// Clears the trigPin
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-// Calculating the distance
- distance = duration*0.034 /2;
-Serial.print("Distance: ");
-Serial.println(distance);
-delay(500);
-randNumber = random(0,4);
-if (distance < 20) {
-digitalWrite(LEDpin, HIGH);
-switch (randNumber) {
-  
-  case 0:
-  lcd.setCursor(1,1);
-  lcd.print("You look great today !");
-  lcd.scrollDisplayLeft();
-  delay(5000);
-  break;
-    
-  case 1:   
-  lcd.setCursor(1,1);
-  lcd.print("You have the best laugh");
-  lcd.scrollDisplayLeft();
-  delay(5000);
-  break;
-  
-  case 2:
-  lcd.setCursor(1,1);
-  lcd.print("You’re a candle in the darkness");
-  lcd.scrollDisplayLeft();
-  delay(5000);
-  break;
-   
-  case 3:
-  lcd.setCursor(1,1);
-  lcd.print("You’re really something special");
-  lcd.scrollDisplayLeft();
-  delay(5000);
-  break;
-   
-  case 4:
-  lcd.setCursor(1,1);
-  lcd.print(" You’re a gift to those around you");
-  lcd.scrollDisplayLeft();
-  delay(5000);
-  break;
-  
-}
- } 
- 
+
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+ // Serial.print("Distance: ");
+  //Serial.println(distance);
+  delay(500);
+  randNumber = random(0, 4);
+  if (distance < 20) {
+    digitalWrite(LEDpin, HIGH);
+    lcd.clear();
+    switch (randNumber) {
+
+      case 0:
+        lcd.setCursor(1, 1);
+        lcd.print("You look pretty!");
+        lcd.scrollDisplayLeft();
+     //   lcd.clear();
+        delay(10000);
+        break;
+
+      case 1:
+        lcd.setCursor(1, 1);
+        lcd.print("I like ur style");
+        lcd.scrollDisplayLeft();
+    //    lcd.clear();
+        delay(10000);
+        break;
+
+      case 2:
+        lcd.setCursor(1, 1);
+        lcd.print("You are special!");
+        lcd.scrollDisplayLeft();
+      //  lcd.clear();
+        delay(10000);
+        break;
+
+      case 3:
+        lcd.setCursor(1, 1);
+        lcd.print("You inspire me!");
+        lcd.scrollDisplayLeft();
+       // lcd.clear();
+        delay(10000);
+        break;
+
+      case 4:
+        lcd.setCursor(1, 1);
+        lcd.print("You are the best");
+        lcd.scrollDisplayLeft();
+        //lcd.clear();
+        delay(10000);
+        break;
+
+    }
+  }
+
   else {
-    
+
+    //lcd.clear();
     digitalWrite(LEDpin, LOW);
-    lcd.setCursor(1,1);
-    lcd.print("Looking for Sentient Being..");
+    lcd.setCursor(1, 1);
+    lcd.print("Looking for Sentient Being.......");
     lcd.scrollDisplayLeft();
     delay(500);
-   
+
   }
 }
